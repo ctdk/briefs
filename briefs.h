31335936741b27e08728932b52f00822b0f71e3e
ctdk <jbingham@gmail.com>
2026-06-06 11:28:12 -0700

    time to bump patch version methinks

--- briefs.h
+++ briefs.h
@@ -26,7 +26,7 @@
 /* Semantic versioning, yo */
 #define _BRIEFS_MAJOR_VER 0
 #define _BRIEFS_MINOR_VER 2
-#define _BRIEFS_PATCH_VER 0
+#define _BRIEFS_PATCH_VER 5
 
 /* Journal magic */
 #define JOURNAL_MAGIC 0x4A4E4C5A  /* "JNLZ" */
