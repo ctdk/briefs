8b2451aacb156bbe86e665d28ea293b7b168fa40
ctdk <jbingham@gmail.com>
2026-06-13 18:43:35 -0700

    bump patch after crc change

--- briefs.h
+++ briefs.h
@@ -28,7 +28,7 @@
 /* Semantic versioning, yo */
 #define _BRIEFS_MAJOR_VER 0
 #define _BRIEFS_MINOR_VER 6
-#define _BRIEFS_PATCH_VER 0
+#define _BRIEFS_PATCH_VER 5
 
 /* Journal magic */
 #define JOURNAL_MAGIC 0x4A4E4C5A  /* "JNLZ" */
